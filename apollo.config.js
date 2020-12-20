c39c1dfc5d6a65672872be2886664fc1c6924d09
Loren 🤓 <lorensr@gmail.com>
2020-12-19 21:19:26 -0500

    Starting template

--- apollo.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  client: {
-    service: {
-      name: 'guide-api',
-      url: 'https://api.graphql.guide/graphql'
-    },
-    includes: ['src/**/*.vue', 'src/**/*.js']
-  }
-}
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <TableOfContents />
+  <h1>The GraphQL Guide</h1>
 </template>
 
 <script>
-import { ApolloClient, InMemoryCache } from '@apollo/client/core'
-import { DefaultApolloClient } from '@vue/apollo-composable'
-import { provide } from 'vue'
-
-import TableOfContents from './components/TableOfContents.vue'
-
-const client = new ApolloClient({
-  uri: 'https://api.graphql.guide/graphql',
-  cache: new InMemoryCache()
-})
-
 export default {
-  name: 'App',
-  components: {
-    TableOfContents
-  },
-  setup() {
-    provide(DefaultApolloClient, client)
-  }
+  name: 'App'
 }
 </script>
