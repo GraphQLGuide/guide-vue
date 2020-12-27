1eb0883ab27ed5c0fd656417d3cf237e52cd40ce
Loren 🤓 <lorensr@gmail.com>
2020-12-27 16:23:21 -0500

    Add apollo client

---
+++ apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'guide-api',
+      url: 'https://api.graphql.guide/graphql'
+    },
+    includes: ['src/**/*.vue', 'src/**/*.js']
+  }
+}
--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,19 @@
 </template>
 
 <script>
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { provide } from 'vue'
+
+const client = new ApolloClient({
+  uri: 'https://api.graphql.guide/graphql',
+  cache: new InMemoryCache()
+})
+
 export default {
-  name: 'App'
+  name: 'App',
+  setup() {
+    provide(DefaultApolloClient, client)
+  }
 }
 </script>
